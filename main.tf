5fc863f80d17cdc7afbbd71b002944641e210250
Enrico Bartz <enrico.bartz@sva.de>
2021-04-29 14:14:25 +0200

    another one

--- main.tf
+++ main.tf
@@ -2,19 +2,19 @@ variable "hcloud_token" {}
 variable "public_key" {}
 variable "image" {}
 variable "name" {}
- 
+
 #Configure the Hetzner Cloud Provider
 provider "hcloud" {
   version = "= 1.10"
   token = "${var.hcloud_token}"
 }
- 
+
 # Create a new SSH key
 resource "hcloud_ssh_key" "key" {
   name = "${var.name}-key"
   public_key = "${var.public_key}"
 }
- 
+
 # Create a new server running debian
 resource "hcloud_server" "node1" {
   name = "${var.name}-node"
@@ -22,7 +22,6 @@ resource "hcloud_server" "node1" {
   server_type = "cx11"
   ssh_keys = ["${var.name}-key"]
 }
-
 
 output "private_ip" {
   value = "${hcloud_server.node1.ipv4_address}"
@@ -35,5 +34,3 @@ output "public_ip" {
 output "hostname" {
   value = "${hcloud_server.node1.name}"
 }
-
- 
