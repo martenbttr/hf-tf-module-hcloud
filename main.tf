ff4e685179eaca4597c53115c9e75a810fd17889
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2023-06-14 11:04:36 +0200

    Remove deprecated version

--- main.tf
+++ main.tf
@@ -17,34 +17,33 @@ terraform {
 
 #Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  version = "= 1.40.0"
-  token = "${var.hcloud_token}"
+  token = var.hcloud_token
 }
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "key" {
   name = "${var.name}-key"
-  public_key = "${var.public_key}"
+  public_key = var.public_key
 }
 
 # Create a new server running debian
 resource "hcloud_server" "node1" {
-  name = "${var.name}"
-  image = "${var.image}"
-  location = "${var.location}"
-  server_type = "${var.server_type}"
+  name = var.name
+  image = var.image
+  location = var.location
+  server_type = var.server_type
   ssh_keys = ["${var.name}-key"]
-  user_data = "${var.cloud-config}"
+  user_data = var.cloud-config
 }
 
 output "private_ip" {
-  value = "${hcloud_server.node1.ipv4_address}"
+  value = hcloud_server.node1.ipv4_address
 }
 
 output "public_ip" {
-  value = "${hcloud_server.node1.ipv4_address}"
+  value = hcloud_server.node1.ipv4_address
 }
 
 output "hostname" {
-  value = "${hcloud_server.node1.name}"
+  value = hcloud_server.node1.name
 }
