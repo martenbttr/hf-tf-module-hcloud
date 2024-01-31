dfed3af81894c49e1033c5de5a020c5e136fe34e
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2024-01-31 10:54:34 +0100

    Allow for labels and firewalls

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ variable "name" {}
 variable "server_type" {}
 variable "location" {}
 variable "cloud-config" {}
+variable "labels" {
+  type        = string
+  default     = ""
+  description = "Optional labels for the server in 'key1=value1,key2=value2' format"
+}
+variable "firewall_ids" {
+  type        = string
+  default     = ""
+  description = "Optional firewall IDs for the server, comma-separated"
+}
 
 terraform {
   required_providers {
@@ -35,6 +45,21 @@ resource "hcloud_server" "node1" {
   server_type = var.server_type
   ssh_keys = ["${var.name}-key"]
   user_data = var.cloud-config
+  
+  dynamic "labels" {
+    for_each = length(var.labels) > 0 ? { for pair in split(",", var.labels) : split("=", pair)[0] => split("=", pair)[1] } : {}
+    content {
+      key   = labels.key
+      value = labels.value
+    }
+  }
+
+  dynamic "firewall" {
+    for_each = length(var.firewall_ids) > 0 ? split(",", var.firewall_ids) : []
+    content {
+      firewall_id = firewall.value
+    }
+  }
 }
 
 output "private_ip" {
